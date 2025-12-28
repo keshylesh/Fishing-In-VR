90aa1c1de4b8505ded48362da0aaa75cdc823d53
Keshav Shylesh <46822111+keshylesh@users.noreply.github.com>
2025-12-28 13:33:33 +0530

    feat: add hand tracking and animations

---
+++ Assets/HandAnimation.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class HandAnimation : MonoBehaviour
+{
+    public InputActionProperty pinch;
+    public InputActionProperty grip;
+    public Animator handAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float triggerValue = pinch.action.ReadValue<float>();
+        handAnimator.SetFloat("Trigger", triggerValue);
+
+        float gripValue = grip.action.ReadValue<float>();
+        handAnimator.SetFloat("Grip", gripValue);
+    }
+}
